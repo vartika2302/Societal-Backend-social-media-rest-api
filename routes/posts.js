f1cfacfeb1cc9635c0aab362a173987b11e4c004
Vartika <guptavartika624@gmail.com>
2021-12-24 21:07:17 +0530

    Add Post model schema

---
+++ routes/posts.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+
+// router.get("/", (req, res) => {
+//   res.send("hello from posts side");
+// });
+
+
+
+module.exports = router;
