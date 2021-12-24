36f75a660969169c1d77e5c9e58460efe6e457ea
Vartika <guptavartika624@gmail.com>
2021-12-24 21:31:33 +0530

    Add create post part

--- models/Post.js
+++ models/Post.js
@@ -17,6 +17,10 @@ const postSchema = new mongoose.Schema(
       type: Array,
       default: [],
     },
+    comments: {
+        type: Array,
+        default: []
+    }
   },
   { timestamps: true }
 );
--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,23 @@ const Post = require("../models/Post");
 //   res.send("hello from posts side");
 // });
 
+//create a post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+//update a post
+
+//delete a post
+//get a post
+//get timeline posts
+//like a post
+//unlike a post
 
 module.exports = router;
