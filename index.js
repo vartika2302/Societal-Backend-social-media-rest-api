b54681feb8a3f0b22a82ed474bb5c4553eb937ec
Vartika <guptavartika624@gmail.com>
2021-12-24 16:14:12 +0530

    Setup API and Add Register user part

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const app = express();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+//MongoDB connection
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to home page");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Welcome to users page"); 
+});
+
+app.listen(8800, () => {
+  console.log("Backend server is running at port 8800");
+});
---
+++ routes/auth.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  // const user = await new User({
+  //   username: "john",
+  //   email: "john@gmail.com",
+  //   password: "123john",
+  // });
+  // await user.save();
+  // res.send("ok");
+  
+  try{
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password,salt);
+    
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and return response
+    const user = await newUser.save();
+    res.status(200).json(user);
+  }catch(err){
+    console.log(err);
+  }
+});
+
+
+//LOGIN
+
+// router.get("/", (req, res) => {
+//   res.send("Hey it's auth route");
+// });
+
+module.exports = router;
