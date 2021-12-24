1fdced59fb513972a015878aa71ba9cea114c1e7
Vartika <guptavartika624@gmail.com>
2021-12-24 20:26:01 +0530

    Add follow user part

--- routes/users.js
+++ routes/users.js
@@ -51,8 +51,32 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-//get all users
 //follow a user
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try{
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if(!user.followers.includes(req.body.userId)){
+        await user.updateOne({$push:{
+          followers:req.body.userId
+        }});
+        await currentUser.updateOne({$push:{
+          following: req.params.id
+        }});
+        res.status(200).json("User has been successfully followed");
+      }
+      else{
+        res.status(403).json("You already follow this user");
+      }
+    }catch(err){
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can't follow yourself");
+  }
+});
+
 //unfollow a user
 
 // router.get("/", (req, res) => {
