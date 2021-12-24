4a95251326bb0bfc0d2d693a3d507df360bec13a
Vartika <guptavartika624@gmail.com>
2021-12-24 16:31:33 +0530

    Add login user part

--- routes/auth.js
+++ routes/auth.js
@@ -11,12 +11,12 @@ router.post("/register", async (req, res) => {
   // });
   // await user.save();
   // res.send("ok");
-  
-  try{
+
+  try {
     //generate new password
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password,salt);
-    
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     //create new user
     const newUser = new User({
       username: req.body.username,
@@ -27,13 +27,28 @@ router.post("/register", async (req, res) => {
     //save user and return response
     const user = await newUser.save();
     res.status(200).json(user);
-  }catch(err){
-    console.log(err);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-
 //LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).send("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(404).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // router.get("/", (req, res) => {
 //   res.send("Hey it's auth route");
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("Hey it's user route");
-});
+
+
+// router.get("/", (req, res) => {
+//   res.send("Hey it's user route");
+// });
 
 module.exports = router;
