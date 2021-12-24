b9d7a2c895ad62e3637f26840ade5d3d9f9bcead
Vartika <guptavartika624@gmail.com>
2021-12-24 23:05:37 +0530

    Add get a post part

--- routes/posts.js
+++ routes/posts.js
@@ -47,6 +47,15 @@ router.delete("/:id", async (req, res) => {
 });
 
 //get a post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //get timeline posts
 
 //like/dislike a post
