281499f49930f7e615b235875916b6746f202032
Vartika <guptavartika624@gmail.com>
2021-12-27 20:13:55 +0530

    Changed database and add conversations & messages routes and models

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 
 dotenv.config();
 
@@ -27,6 +29,8 @@ app.use(morgan("common"));
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
 app.use("/api/posts",postRoute);
+app.use("/api/conversations",conversationRoute);
+app.use("/api/messages",messageRoute);
 
 app.get("/", (req, res) => {
   res.send("Welcome to home page");
